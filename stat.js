09c152e2bc859e9bf343ecfcf8edd3303d85cb00
Shuaibird <shuaibird@icloud.com>
2017-07-11 14:11:30 +0800

    [ADD] stat.js

---
+++ stat.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+const formatTime = tsp => {
+  const hours = tsp / (1000 * 60 * 60)
+  const days = Math.floor(hours / 24)
+  return `${days && `${days} days`} ${Math.floor(hours % 24)} hours`
+}
+
+fs.readFile(path.resolve(__dirname, 'backup.txt'), (err, file) => {
+  const now = +new Date()
+  const ports = file
+    .toString()
+    .split('\n')
+    .map(line => JSON.parse(line))
+    .map(({ server_port: port, expired_on }) => ({
+      port,
+      expired: formatTime(expired_on * 1000 - now),
+    }))
+  console.log(ports)
+})
